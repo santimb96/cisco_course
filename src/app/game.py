cad13da079362eac4137445302eeb8fd6b516319
santimb96 <santiagomartinezbota@gmail.com>
2022-10-07 21:46:14 +0200

    some bugs fixed, some controls added and warnings and some code has been refactored

--- src/app/game.py
+++ src/app/game.py
@@ -1,4 +1,5 @@
-import random
+from random import randrange
+from termcolor import colored
 
 
 def init_game():
@@ -18,12 +19,14 @@ def init_game():
     cpu_moves = []
     user_moves = []
 
+    game_over_message = colored(f"¡Has perdido!", "red")
+    win_message = colored(f"¡Has ganado!", "green")
+
     turn_number = 0
 
-    # 1
     def print_board():
         for row in board:
-            print(row)
+            print(f"| {row[0]} - {row[1]} - {row[2]} |")
 
     def check_number_in_range(n):
         if n > 9 or n < 1:
@@ -32,11 +35,13 @@ def init_game():
         return True
 
     def select_option_on_board(option, user):
-        board[option[0]][option[1]] = "o" if user else "x"
+        board[option[0]][option[1]] = (
+            colored("0", "green") if user else colored("X", "red")
+        )
         return
 
     def random_move():
-        return random.randrange(1, 9)
+        return randrange(1, 9)
 
     def check_availability(option):
         for row in board:
@@ -59,12 +64,40 @@ def init_game():
 
         return False
 
+    def check_if_game_ended(moves=[], user=False):
+        if turn_number >= 3 and check_win(moves):
+            print(win_message if user else game_over_message)
+            print_board()
+            return quit()
+        if turn_number == 5:
+            print(colored("¡Empate!", "blue"))
+            print_board()
+            return quit()
+
+        return
+
     def user_move():
 
         option = False
+        attempts = 0
+        while (
+            check_number_in_range(option) == False
+            or (option in user_moves)
+            or (option in cpu_moves)
+        ):
+            attempts += 1
 
-        while check_number_in_range(option) == False or (option in user_moves):
-            option = int(input("Escoge casilla(por número): "))
+            if attempts > 1:
+                print(colored("¡Aségurate de escoger un número libre!", "yellow"))
+            try:
+                option = int(input("Escoge casilla(por número): "))
+            except:
+                print(
+                    colored(
+                        "Has de ingresar un número del 1 al 9 (ambos incluidos)",
+                        "yellow",
+                    )
+                )
 
         availability = check_availability(option)
 
@@ -72,9 +105,7 @@ def init_game():
 
         user_moves.append(option)
 
-        if turn_number >= 3 and check_win(user_moves):
-            print("Has ganado con " + str(turn_number))
-            return print_board()
+        check_if_game_ended(user_moves, True)
         return init_turn()
 
     # 2 movimiento de la CPU
@@ -95,22 +126,19 @@ def init_game():
 
         select_option_on_board(availability, False)
 
-        if turn_number >= 3 and check_win(cpu_moves):
-            print("Has perdido con " + str(turn_number))
-            return print_board()
+        check_if_game_ended(cpu_moves, False)
 
         print("============")
         print_board()
         return user_move()
 
     def init_turn():
-        nonlocal turn_number
-        turn_number += 1
+        nonlocal turn_number #declaramos que el ámbito de la variable es global
+        turn_number += 1 #sumamos turno
         print("============")
-        print_board()
-        return cpu_move()
+        print_board() #pintamos el tablero
+        return cpu_move() #inicia siempre CPU
 
     init_turn()
-
 
 init_game()
